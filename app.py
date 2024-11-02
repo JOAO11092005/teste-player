6d143d3ea917a97a42b9d873c7d4fd0a32e6658a
Joao Paulo Neves <100012505+JOAO11092005@users.noreply.github.com>
2024-11-02 15:50:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
-from flask import Flask, Response, request
-import requests
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-VIDEO_URL = "http://cdn.teambr.live:80/movie/iptv0887/19971460887/49856.mp4"
-
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-@app.route('/video')
-def video():
-    response = requests.get(VIDEO_URL, stream=True)
-    return Response(response.iter_content(chunk_size=4096), 
-                    content_type=response.headers['Content-Type'])
-
 if __name__ == '__main__':
-    app.run(ssl_context='adhoc')  # Use SSL para HTTPS
+    # Removido o ssl_context para rodar sem HTTPS
+    app.run(host='0.0.0.0', port=5000)
